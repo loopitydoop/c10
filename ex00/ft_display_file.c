8f4e224c26fd248c5442e8e77231e6192bf910d6
Shuin Kang Wu <shuwu@c2r5s12.42singapore.sg>
2026-07-16 14:34:11 +0800

    updated ft_display_file.c, I think it generally works! Work on error messages next

--- ex00/ft_display_file.c
+++ ex00/ft_display_file.c
@@ -6,15 +6,43 @@
 /*   By: shuwu <shuwu@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 11:10:27 by shuwu             #+#    #+#             */
-/*   Updated: 2026/07/16 11:13:06 by shuwu            ###   ########.fr       */
+/*   Updated: 2026/07/16 14:32:19 by shuwu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h> // for open() flag definitions
+#include <sys/stat.h> // for open() file permission macros?
 #include <unistd.h>
 
 // displays the CONTENT of the file given as a argument
 // e.g. ./ft_display_file <filename> would display the contents of <filename>
+// try man -s3 open to figure out how to open files.
+// open(pathname, flag)
+// basic flags include O_RDONLY, O_WRONLY, O_RDWR
+// 	additional flags can be added with the bitwise OR operator | 
+//	O_CREAT: create file if it doesn't alr exist (if it alr exists,
+//		it will just open a file
+//	O_TRUNC: truncate the file length to 0 bytes if it alr exists
+//	O_APPEND: force write operations to append to the end of the file
+//	O_EXCL: use with O_CREAT to fail the operation if the file alr exists
+//	additional octal args  exist if you wanna use O_CREAT, they are 
+// 		regarding file permissions.
+//	open returns -1 on failure, otherwise returns a "file descriptor"
+//		which is like a index number in the kernel's process for a
+//		specific file
+//	read() returns the number of bytes successfully read.
+
 int	main(int argc, char **argv)
 {
+	int	fd;
+	char	buf[1];	
+
+	(void)argc;
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (1);
+	while(read(fd, buf, 1) > 0)
+		write(1, buf, 1);
+	close(fd);
 	return (0);
 }
