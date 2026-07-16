823ef6e8a5217100ef3f4da8922278172ffb0d86
Shuin Kang Wu <shuwu@c2r5s12.42singapore.sg>
2026-07-16 11:14:27 +0800

    added ft_display_file.c. Maybe it should be main.c instead?

---
+++ ex00/ft_display_file.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shuwu <shuwu@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/16 11:10:27 by shuwu             #+#    #+#             */
+/*   Updated: 2026/07/16 11:13:06 by shuwu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+// displays the CONTENT of the file given as a argument
+// e.g. ./ft_display_file <filename> would display the contents of <filename>
+int	main(int argc, char **argv)
+{
+	return (0);
+}
